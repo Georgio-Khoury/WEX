0bf16269e4d73c7553947afc48a9528f865470d9
Georgio Khoury <georgiok033@hotmail.com>
2024-05-04 19:21:14 +0300

    added base

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://wex-frontend.onrender.com/',
   server: {
     proxy: {
       "/api": {
         target: "https://wex-1.onrender.com/",
-
+        
         changeOrigin:true,
         secure: true,
           //  target: "http://localhost:3001",
