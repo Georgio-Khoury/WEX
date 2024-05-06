fed62e6cfae880a2d1299addd47c11d206645299
Georgio Khoury <georgiok033@hotmail.com>
2024-05-06 21:35:56 +0300

    v

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -15,7 +15,7 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, { 
-    user: null
+    user: sessionStorage.getItem('username')
   })
 
   useEffect(() => {
